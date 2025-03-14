44f394d293822a01532074b25b6247df5b23c5ec
Gautam Tayal <gautamtayal65@gmail.com>
2025-03-14 13:23:20 +0530

    kinde auth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
-import { HomeIcon, File, UsersRound } from "lucide-react";
+import { HomeIcon, File, UsersRound, LogOut  } from "lucide-react";
 import Link from "next/link";
 import { NavButton } from "./NavButton";
 import { ModeToggle } from "./ModeToogle";
-
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "./ui/button";
 
 export default function Header() {
   return (
-    <header className="animate-in bg-background px-4 py-2 h-12 sticky z-20">
-      <div className="flex h-8 items-center justify-between w-full">
+    <header className="animate-in bg-background px-4 py-2 h-12 sticky z-20 ">
+      <div className="flex h-8 items-center justify-between w-full ">
         <div className="flex items-center gap-2">
         <NavButton href= "/home" label="Home" icon={HomeIcon}/>  
         <Link href="/home" className="flex justify-center gap-2 ml-0" title="Home">
@@ -19,6 +20,20 @@ export default function Header() {
           <div className="flex items-center">
           <NavButton href= "/tickets" label="Tickets" icon={File}/> <NavButton href= "/customers" label="Customers" icon={UsersRound}/> 
           <ModeToggle />
+
+          <Button 
+            variant="ghost"
+            size="icon"
+            aria-label="LogOut"
+            title="Logout"
+            className="rounded-full"
+            asChild
+            >
+              <LogoutLink>
+                <LogOut />
+              </LogoutLink>
+            </Button>
+
           </div> 
       </div> 
     </header>
